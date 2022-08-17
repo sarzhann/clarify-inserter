2fb2fde6b5ca28d8f6a8ff70f4236ed3972827ea
Nurzhan Sarzhan <nurzhan@clarify.io>
2022-08-17 14:43:20 +0200

    refactor

--- inserter/inserter.go
+++ inserter/inserter.go
@@ -22,41 +22,43 @@ type Creds struct {
 
 type Inserter struct {
 	c            *clarify.Client
+	frequency    time.Duration
+	diff         time.Duration
+	value        float64
 	server       string
 	method       string
 	item         string
-	value        float64
 	itemsCount   int
 	signalsCount int
 	bucket       bool
-	frequency    time.Duration
-	diff         time.Duration
+	sinWave      bool
 }
 
 func New(ctx context.Context) (*Inserter, error) {
-	server := flag.String("server", "local", "choose environment variable name. Stored under envs/<server>.env")
-	method := flag.String("method", "stream", "choose between 'stream', 'single'. Default:'stream'")
-	item := flag.String("item", "data", "item to which write data. Default:'data'")
-	value := flag.Float64("value", -999.0, "choose value for inserting. Works only with 'single' method. Random for 'stream'")
-	itemsCount := flag.Int("items", 10, "Number of items per insert (Applicable only if 'bucket' is enabled")
-	signalsCount := flag.Int("signals", 10, "Number of signals per item per insert (Applicable only if 'bucket' is enabled")
-	bucket := flag.Bool("bucket", false, "to insert a bucket of data")
-	frequency := flag.Int("frequency", 15, "period of inserting data into clarify (in seconds). Applicable only for 'stream' method. Default:15")
-	diff := flag.Int("diff", 0, "minutes before now. Default:0")
+	var ins Inserter
+	flag.DurationVar(&ins.frequency, "frequency", 15*time.Second, "period of inserting data into clarify (in seconds). Applicable only for 'stream' method. Default:15")
+	flag.DurationVar(&ins.diff, "diff", 0*time.Minute, "minutes before now. Default:0")
+	flag.Float64Var(&ins.value, "value", -999.0, "choose value for inserting. Works only with 'single' method. Random for 'stream'")
+	flag.StringVar(&ins.server, "server", "local", "choose environment variable name. Stored under envs/<server>.env")
+	flag.StringVar(&ins.method, "method", "stream", "choose between 'stream', 'single'. Default:'stream'")
+	flag.StringVar(&ins.item, "item", "data", "item to which write data. Default:'data'")
+	flag.IntVar(&ins.itemsCount, "items", 10, "Number of items per insert (Applicable only if 'bucket' is enabled")
+	flag.IntVar(&ins.signalsCount, "signals", 10, "Number of signals per item per insert (Applicable only if 'bucket' is enabled")
+	flag.BoolVar(&ins.bucket, "bucket", false, "to insert a bucket of data")
+	flag.BoolVar(&ins.sinWave, "sinWave", false, "To use sinus wave pattern for insertion")
 	flag.Parse()
 
-	err := godotenv.Load("envs/" + *server + ".env")
+	err := godotenv.Load("envs/" + ins.server + ".env")
 	if err != nil {
 		return nil, err
 	}
-
 	creds := Creds{
 		Url:           os.Getenv("APIURL"),
 		IntegrationID: os.Getenv("CLARIFY_INTEGRATION_ID"),
 		Password:      os.Getenv("CLARIFY_PASSWORD"),
 	}
 
-	c := clarify.Credentials{
+	ins.c = clarify.Credentials{
 		APIURL:      creds.Url,
 		Integration: creds.IntegrationID,
 		Credentials: clarify.CredentialsAuth{
@@ -64,36 +66,36 @@ func New(ctx context.Context) (*Inserter, error) {
 			ClientID:     creds.IntegrationID,
 			ClientSecret: creds.Password,
 		},
-	}
+	}.Client(ctx)
 
-	return &Inserter{
-		c:            c.Client(ctx),
-		server:       *server,
-		method:       *method,
-		item:         *item,
-		value:        *value,
-		itemsCount:   *itemsCount,
-		signalsCount: *signalsCount,
-		bucket:       *bucket,
-		frequency:    time.Duration(*frequency),
-		diff:         time.Duration(*diff),
-	}, nil
+	return &ins, nil
+}
+
+func sleep(err error) {
+	log.Printf("Failed to insert data: %v", err)
+	time.Sleep(5 * time.Second)
 }
 
 func (inserter *Inserter) Run(ctx context.Context) {
-	log.Printf("Start inserter")
+	log.Printf("Starting inserter...")
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
 	switch {
 	case inserter.method == "single":
-		ts := fields.AsTimestamp(time.Now().Add(-inserter.diff * time.Minute))
-		if err := inserter.insertSingle(ctx, ts); err != nil {
-			log.Fatalf("Failed to insert data: %v", err)
+		var err error
+		attempts := 5
+		ts := fields.AsTimestamp(time.Now().Add(-inserter.diff))
+		for i := 0; i < attempts; i++ {
+			err = inserter.insertSingle(ctx, ts)
+			if err == nil {
+				return
+			}
+			sleep(err)
 		}
 
 	case inserter.method == "stream":
-		ticker := time.NewTicker(inserter.frequency * time.Second)
+		ticker := time.NewTicker(inserter.frequency)
 		for {
 			select {
 			case <-ctx.Done():
@@ -101,26 +103,26 @@ func (inserter *Inserter) Run(ctx context.Context) {
 			case tick := <-ticker.C:
 				if inserter.bucket {
 					if err := inserter.insertBucket(ctx); err != nil {
-						log.Printf("Failed to insert data: %v", err)
-						time.Sleep(5 * time.Second)
+						sleep(err)
 						continue
 					}
 				} else {
 					ts := fields.AsTimestamp(tick)
-					inserter.value = 100 * (rand.NormFloat64()/2 + math.Sin((float64(tick.Second()) / (math.Pi * math.Pi))))
+					if inserter.sinWave {
+						inserter.value = 100 * (rand.NormFloat64()/2 + math.Sin((float64(tick.Second()) / (math.Pi * math.Pi))))
+					} else {
+						inserter.value = 100 * rand.Float64()
+					}
 					if err := inserter.insertSingle(ctx, ts); err != nil {
-						log.Printf("Failed to insert data: %v", err)
-						time.Sleep(5 * time.Second)
+						sleep(err)
 						continue
 					}
 				}
-
 			}
 		}
 	default:
 		log.Fatal("method not in [single, stream]")
 	}
-	log.Printf("Finish inserter")
 }
 
 func (inserter *Inserter) insertSingle(ctx context.Context, ts fields.Timestamp) error {
